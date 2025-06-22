5c3531100045eaadbe73cccacf0c3302fea7a37f
Alex Hatfield <aldmbmtl@users.noreply.github.com>
2025-06-22 16:02:30 -0400

    71-allow-read-only-custom-scripts (#72)
    * pr commit
    
    * Adding in support for Read-Only file systems.
    
    * Adding in support for nested entry point plugins.
    
    ---------
    
    Co-authored-by: app/github-actions <app/github-actions>
    Co-authored-by: aldmbmtl <alex.e.hatfield@gmail.com>

--- common/root/etc/s6-overlay/s6-rc.d/init/start.sh
+++ common/root/etc/s6-overlay/s6-rc.d/init/start.sh
@@ -7,14 +7,26 @@ cat /opt/helios/header.txt
 echo "Helios Version $HELIOS_VERSION"
 cat /opt/helios/*.version
 
-chmod +x /etc/helios/init.d/*.sh
+chmod +x /etc/helios/init.d/*.sh || echo "Potentially Read-Only Mounts. Skipping chmod. Root..."
+chmod +x /etc/helios/init.d/*/entrypoint.sh || echo "Potentially Read-Only Mounts. Skipping chmod. Nested..."
 
 for script in /etc/helios/init.d/*.sh; do
 	if [ -x "$script" ]; then
 		echo "Running custom init script: $script"
 		"$script"
 	else
-		echo "Skipping non-executable script: $script"
+		printf "Non-executable script: $script\n\tAttempting with bash: /bin/bash $script\n"
+		/bin/bash "$script" || echo "Failed to run: $script"
+	fi
+done
+
+for script in /etc/helios/init.d/*/entrypoint.sh; do
+	if [ -x "$script" ]; then
+		echo "Running custom init script: $script"
+		"$script"
+	else
+		printf "Non-executable script: $script\n\tAttempting with bash: /bin/bash $script\n"
+		/bin/bash "$script" || echo "Failed to run: $script"
 	fi
 done
 
