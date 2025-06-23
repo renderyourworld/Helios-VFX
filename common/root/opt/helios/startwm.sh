3804aeb18d0c0c9c69fa19b276739798fc05aed5
Alex Hatfield <aldmbmtl@users.noreply.github.com>
2025-06-22 21:53:22 -0400

    69-update-theme (#70)
    * Updated theme.
    
    * Size updated
    
    * Updated theme to have proper branding.
    
    ---------
    
    Co-authored-by: app/github-actions <app/github-actions>
    Co-authored-by: aldmbmtl <alex.e.hatfield@gmail.com>

--- common/root/opt/helios/startwm.sh
+++ common/root/opt/helios/startwm.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 if [ -x /usr/bin/xfce4-session ]; then
-	# Disable compositing
-	if [ -f "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml ]; then
-		sed -i \
-			'/use_compositing/c <property name="use_compositing" type="bool" value="false"/>' \
-			"${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
-	fi
 	/usr/bin/xfce4-session 2>&1
 else
 	echo "Desktop Environment not found."
