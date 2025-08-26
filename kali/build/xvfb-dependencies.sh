6c6978ac051b02e0a6fea612166c7c1e21cd11cf
aldmbmtl <alex.e.hatfield@gmail.com>
2025-08-26 14:36:48 -0400

    Cleaning out linter

--- kali/build/xvfb-dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# add srcs for deb
-echo "deb http://http.kali.org/kali kali-rolling main contrib non-free non-free-firmware" >>/etc/apt/sources.list
-echo "deb-src http://http.kali.org/kali kali-rolling main contrib non-free non-free-firmware" >>/etc/apt/sources.list
